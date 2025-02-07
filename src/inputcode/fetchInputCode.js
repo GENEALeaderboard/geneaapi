b9720f1890ef3c39e981bd129402501698df223e
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-02-07 16:10:26 +0700

    update

--- src/inputcode/fetchInputCode.js
+++ src/inputcode/fetchInputCode.js
@@ -4,13 +4,13 @@ export async function fetchInputCode(request, env, corsHeaders) {
 	try {
 		// const result = {}
 		const db = env.DB_HEMVIP
-		const result = await db.prepare("SELECT * FROM inputcode").all()
-		console.log("result", result)
-		if (!result) {
+		const response = await db.prepare("SELECT * FROM inputcode").all()
+
+		if (!response.result) {
 			return responseFailed("No studies found", 404, corsHeaders)
 		}
 
-		return responseSuccess({ codes: result }, corsHeaders)
+		return responseSuccess({ codes: response.result }, corsHeaders)
 	} catch (err) {
 		console.error("Exception", err)
 		return responseError(err, "Exception", 500, corsHeaders)
