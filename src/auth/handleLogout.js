59a17a8e89d521154bc14e6e23e07182af62bedc
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-02-07 14:52:43 +0700

    update logout

--- src/auth/handleLogout.js
+++ src/auth/handleLogout.js
@@ -3,10 +3,6 @@ export async function handleLogout(request, env, corsHeaders) {
 	const secure = env.ENVIRONMENT === "development" ? "SameSite=Lax;" : "Secure; SameSite=None;"
 
 	response.headers.set("Set-Cookie", `genea-auth-token=; Path=/; HttpOnly; ${secure} Max-Age=0`)
-	response.headers.set(
-		"Set-Cookie",
-		`genea-api-token=; Domain=${env.NEXT_PUBLIC_API_ENDPOINT}; Path=/; HttpOnly; Secure; SameSite=None; Max-Age=${24 * 60 * 60}`
-	)
 
 	return response
 }
