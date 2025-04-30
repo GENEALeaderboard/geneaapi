a5efaf8cc8f821e2bfa4bb689bf6ca85a90e629d
Thanh Hoang-Minh <hmthanhgm@gmail.com>
2025-04-30 13:50:23 +0700

    fix success submission

--- src/submissions/fetchSubmissionFiltered.js
+++ src/submissions/fetchSubmissionFiltered.js
@@ -2,7 +2,7 @@ import { responseError, responseFailed, responseSuccess } from "../response"
 
 export async function fetchSubmissionFiltered(request, db, corsHeaders) {
 	try {
-		const response = await db.prepare("SELECT * FROM submissions s WHERE NOT EXISTS (SELECT 1 FROM systems y WHERE y.submissionid = s.id)").all()
+		const response = await db.prepare("SELECT * FROM submissions s WHERE NOT EXISTS (SELECT 1 FROM systems y WHERE y.submissionid = s.id) AND s.status = 'success'").all()
 
 		if (!response.results) {
 			return responseFailed(null, "No submissions found", 404, corsHeaders)
