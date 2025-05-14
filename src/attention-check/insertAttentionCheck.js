fb11fb4cc0b4417a5b5f3819e68d861d74db2e62
MikeTsakov <tsakovm@gmail.com>
2025-05-14 13:57:58 +0200

    add type and volume for database

--- src/attention-check/insertAttentionCheck.js
+++ src/attention-check/insertAttentionCheck.js
@@ -32,8 +32,8 @@ export async function insertAttentionCheck(request, db, corsHeaders) {
 		}
 
 		const stmtAttentionCheck = await db.prepare(
-			`INSERT INTO attentioncheck (url1, path1, url2, path2, expected_vote, videoid1, videoid2)
-			VALUES (?, ?, ?, ?, ?, ?, ?)`
+			`INSERT INTO attentioncheck (url1, path1, url2, path2, expected_vote, videoid1, videoid2, type, volume)
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
 		)
 		console.log("pairAttentionCheck", pairAttentionCheck)
 		const batchAttentionCheck = []
@@ -42,8 +42,8 @@ export async function insertAttentionCheck(request, db, corsHeaders) {
 				console.log("attentionCheck", attentionCheck)
 				return responseFailed(null, `Attention check meta data not found for idx: ${idx}`, 400, corsHeaders)
 			}
-			const { path: path1, url: url1, expectedVote: expectedVote1, videoid: videoid1 } = attentionCheck[0]
-			const { path: path2, url: url2, expectedVote: expectedVote2, videoid: videoid2 } = attentionCheck[1]
+			const { path: path1, url: url1, expectedVote: expectedVote1, videoid: videoid1, type: type1, volume: volume1 } = attentionCheck[0]
+			const { path: path2, url: url2, expectedVote: expectedVote2, videoid: videoid2, type: type2, volume: volume2 } = attentionCheck[1]
 
 			let expectedVote = expectedVote1
 			if (expectedVote1 === "Reference") {
@@ -54,7 +54,7 @@ export async function insertAttentionCheck(request, db, corsHeaders) {
 				return responseFailed(null, `Expected vote not found for idx: ${idx}`, 400, corsHeaders)
 			}
 
-			batchAttentionCheck.push(stmtAttentionCheck.bind(url1, path1, url2, path2, expectedVote, videoid1, videoid2))
+			batchAttentionCheck.push(stmtAttentionCheck.bind(url1, path1, url2, path2, expectedVote, videoid1, videoid2, type2, volume2))
 		}
 		const attentionCheckResults = await db.batch(batchAttentionCheck)
 		const successAll = Array.from(attentionCheckResults).every((result) => result.success)
